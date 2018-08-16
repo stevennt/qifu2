d52ade0c701ea2fb74d57dca129f62d502cfc9f7
billchen198318 <chen.xin.nien@gmail.com>
2018-08-16 10:01:19 +0800

    Update add AD login custom query attribute realm and config.

--- core-base/src/org/qifu/sys/GreenStepBaseAuthorizingActiveDirectoryRealm.java
+++ core-base/src/org/qifu/sys/GreenStepBaseAuthorizingActiveDirectoryRealm.java
@@ -43,7 +43,9 @@ import org.springframework.beans.factory.annotation.Required;
 
 public class GreenStepBaseAuthorizingActiveDirectoryRealm extends ActiveDirectoryRealm {
 	private IUserRoleService<UserRoleVO, TbUserRole, String> userRoleService;
-	private IRolePermissionService<RolePermissionVO, TbRolePermission, String> rolePermissionService;	
+	private IRolePermissionService<RolePermissionVO, TbRolePermission, String> rolePermissionService;
+	
+	private String customQueryAttributeValue = "sAMAccountName={0}"; // for GreenStepBaseAuthorizingActiveDirectoryCustomQueryAttributeRealm
 	
 	public GreenStepBaseAuthorizingActiveDirectoryRealm() {
 		super();
@@ -71,6 +73,14 @@ public class GreenStepBaseAuthorizingActiveDirectoryRealm extends ActiveDirector
 		this.rolePermissionService = rolePermissionService;
 	}	
 	
+	public String getCustomQueryAttributeValue() {
+		return customQueryAttributeValue;
+	}
+
+	public void setCustomQueryAttributeValue(String customQueryAttributeValue) {
+		this.customQueryAttributeValue = customQueryAttributeValue;
+	}
+
 	private SimpleAuthorizationInfo getSimpleAuthorizationInfo(String username) throws Exception {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("account", username);
